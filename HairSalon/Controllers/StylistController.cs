f99d5abbe501cc3f03605ab98d2d865ea5fa6aef
BrandonF55 <brandon.fenk99@gmail.com>
2022-12-23 10:44:18 -0800

    have controllers for stylist

---
+++ HairSalon/Controllers/StylistController.cs
@@ -0,0 +1,78 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using HairSalon.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HairSalon.Controllers
+{
+  public class HairSalonController : Controller
+  {
+    private readonly HairSalonContext _db;
+
+    public HairSalonController(HairSalonContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Stylist> model = _db.Stylist.ToList();
+      return View (model);
+    }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+      [HttpPost]
+
+      public ActionResult Create(Stylist stylist)
+      {
+        _db.Stylist.Add(stylist);
+        _db.SaveChanges();
+        return RedirectToAction("Idex");
+      }
+
+      public ActionResult Details(int id)  // ?
+    {
+      Stylist thisStylist = _db.Stylist
+                                        .Include(stylist => stylist.Clients)
+                                        .FirstOrDefault(stylist => stylist.StylistId == id);
+      return View(thisStylist);
+    }
+
+    public ActionResult Edit( int id)
+    {
+      Stylist thisStylist = _db.Stylist.FirstOrDefault(stylist => stylist.StylistId == id);
+      return View(thisStylist);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Stylist stylist)
+    {
+      _db.Stylist.Update(stylist);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+      public ActionResult Delete(int id)
+      {
+        Stylist thisStylist = _db.Stylist.FirstOrDefault(stylist => stylist.StylistId == id);
+        return View(thisStylist);
+      }
+
+      [HttpPost, ActionName("Delete")]
+      public ActionResult DeleteConfirmed(int id)
+      {
+        Stylist thisStylist = _db.Stylist.FirstOrDefault(stylist => stylist.StylistId == id);
+        _db.Stylist.Remove(thisStylist);
+        _db.SaveChanges();
+        return RedirectToAction("Index");
+      }
+
+
+  }
+}
+
